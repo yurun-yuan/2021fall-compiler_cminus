ee52b295eaf5a11f0d0d5ce7b13ba4299bc43bf1
Ryan <ryanyuan.yyr@outlook.com>
2021-10-22 18:54:56 +0800

    Remove redundant comments

--- tests/2-ir-gen-warmup/stu_cpp/fun_generator.cpp
+++ tests/2-ir-gen-warmup/stu_cpp/fun_generator.cpp
@@ -8,8 +8,8 @@
 #include <iostream>
 #include <memory>
 
-#ifdef DEBUG                                             // 用于调试信息,大家可以在编译过程中通过" -DDEBUG"来开启这一选项
-#define DEBUG_OUTPUT std::cout << __LINE__ << std::endl; // 输出行号的简单示例
+#ifdef DEBUG
+#define DEBUG_OUTPUT std::cout << __LINE__ << std::endl;
 #else
 #define DEBUG_OUTPUT
 #endif
@@ -18,11 +18,11 @@
     ConstantInt::get(num, module)
 
 #define CONST_FP(num) \
-    ConstantFP::get(num, module) // 得到常数值的表示,方便后面多次用到
+    ConstantFP::get(num, module)
 
 int main()
 {
-    auto module = new Module("Cminus code"); // module name是什么无关紧要
+    auto module = new Module("Cminus code");
     auto builder = new IRBuilder(nullptr, module);
     Type *Int32Type = Type::get_int32_type(module);
 
