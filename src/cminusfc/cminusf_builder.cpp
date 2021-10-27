8f034dc8e6e4feff8e9941927ef0feb1d6d4e97d
Ryan <ryanyuan.yyr@outlook.com>
2021-10-28 07:49:01 +0800

    FInish visit for ASTProgram, ASTVarDeclaration

--- src/cminusfc/cminusf_builder.cpp
+++ src/cminusfc/cminusf_builder.cpp
@@ -6,6 +6,37 @@
 #define CONST_ZERO(type) \
     ConstantZero::get(var_type, module.get())
 
+/**
+ * Return module;
+ * The type is Module *
+ */
+#define MOD this->module.get()
+
+#define GET_INT32 Type::get_int32_type(MOD)
+#define GET_FLOAT Type::get_float_type(MOD)
+#define GET_VOID Type::get_void_type(MOD)
+
+/**
+ * @brief Convert `enum CminusType` to `Type *`
+ * @arg CminusType: a `enum CminusType` value
+ * @arg res: a `Type *` value. The converted value will be assigned to it.
+ * 
+ */
+#define CT2T(CminusType, res) \
+    switch (CminusType)       \
+    {                         \
+    case TYPE_INT:            \
+        res = GET_INT32;      \
+        break;                \
+    case TYPE_FLOAT:          \
+        res = GET_FLOAT;      \
+    case TYPE_VOID:           \
+        res = GET_VOID;       \
+    default:                  \
+        break;                \
+    }
+
+#define INIT(Ty) ConstantZero::get((Ty), MOD)
 
 // You can define global variables here
 // to store state
@@ -18,34 +49,53 @@
  * scope.find: find and return the value bound to the name
  */
 
-void CminusfBuilder::visit(ASTProgram &node) { }
+void CminusfBuilder::visit(ASTProgram &node)
+{
+    this->scope.enter();
+    for (auto &&declaration : node.declarations)
+    {
+        declaration->accept(*this);
+    }
+}
 
-void CminusfBuilder::visit(ASTNum &node) { }
+void CminusfBuilder::visit(ASTNum &node) {}
 
-void CminusfBuilder::visit(ASTVarDeclaration &node) { }
+void CminusfBuilder::visit(ASTVarDeclaration &node)
+{
+    Type *baseType, *finalType;
+    CT2T(node.type, baseType)
+    if (node.num) // is an array
+        finalType = ArrayType::get(baseType, node.num->i_val);
+    else
+        finalType = baseType;
+    if (this->scope.in_global())
+        this->scope.push(node.id, GlobalVariable::create(node.id, MOD, finalType, false, INIT(finalType)));
+    else
+        this->scope.push(node.id, this->builder->create_alloca(finalType));
+}
 
-void CminusfBuilder::visit(ASTFunDeclaration &node) { }
+void CminusfBuilder::visit(ASTFunDeclaration &node){}
 
-void CminusfBuilder::visit(ASTParam &node) { }
+void CminusfBuilder::visit(ASTParam &node) {}
 
-void CminusfBuilder::visit(ASTCompoundStmt &node) { }
+void CminusfBuilder::visit(ASTCompoundStmt &node) {}
 
-void CminusfBuilder::visit(ASTExpressionStmt &node) { }
+void CminusfBuilder::visit(ASTExpressionStmt &node) {}
 
-void CminusfBuilder::visit(ASTSelectionStmt &node) { }
+void CminusfBuilder::visit(ASTSelectionStmt &node) {}
 
-void CminusfBuilder::visit(ASTIterationStmt &node) { }
+void CminusfBuilder::visit(ASTIterationStmt &node) {}
 
-void CminusfBuilder::visit(ASTReturnStmt &node) { }
+void CminusfBuilder::visit(ASTReturnStmt &node) {}
 
-void CminusfBuilder::visit(ASTVar &node) { }
+void CminusfBuilder::visit(ASTVar &node) {}
 
-void CminusfBuilder::visit(ASTAssignExpression &node) { }
+void CminusfBuilder::visit(ASTAssignExpression &node) {}
 
-void CminusfBuilder::visit(ASTSimpleExpression &node) { }
+void CminusfBuilder::visit(ASTSimpleExpression &node) {}
 
-void CminusfBuilder::visit(ASTAdditiveExpression &node) { }
+void CminusfBuilder::visit(ASTAdditiveExpression &node) {}
 
-void CminusfBuilder::visit(ASTTerm &node) { }
+void CminusfBuilder::visit(ASTTerm &node) {}
 
-void CminusfBuilder::visit(ASTCall &node) { }
+void CminusfBuilder::visit(ASTCall &node) {}
