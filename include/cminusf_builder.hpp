98f498b5e2196541e696eebc3cf85fc8686dbe18
Ryan <ryanyuan.yyr@outlook.com>
2021-10-28 21:00:00 +0800

    Add cal_stack to evaluate expressions

--- include/cminusf_builder.hpp
+++ include/cminusf_builder.hpp
@@ -8,37 +8,44 @@
 #include "Type.h"
 #include "ast.hpp"
 #include <map>
+#include <stack>
 
-
-
-class Scope {
+class Scope
+{
 public:
     // enter a new scope
-    void enter() {
+    void enter()
+    {
         inner.push_back({});
     }
 
     // exit a scope
-    void exit() {
+    void exit()
+    {
         inner.pop_back();
     }
 
-    bool in_global() {
+    bool in_global()
+    {
         return inner.size() == 1;
     }
 
     // push a name to scope
     // return true if successful
     // return false if this name already exits
-    bool push(std::string name, Value *val) {
+    bool push(std::string name, Value *val)
+    {
         auto result = inner[inner.size() - 1].insert({name, val});
         return result.second;
     }
 
-    Value* find(std::string name) {
-        for (auto s = inner.rbegin(); s!= inner.rend();s++) {
+    Value *find(std::string name)
+    {
+        for (auto s = inner.rbegin(); s != inner.rend(); s++)
+        {
             auto iter = s->find(name);
-            if (iter != s->end()) {
+            if (iter != s->end())
+            {
                 return iter->second;
             }
         }
@@ -50,10 +57,11 @@ private:
     std::vector<std::map<std::string, Value *>> inner;
 };
 
-
-class CminusfBuilder: public ASTVisitor {
+class CminusfBuilder : public ASTVisitor
+{
 public:
-    CminusfBuilder() {
+    CminusfBuilder()
+    {
         module = std::unique_ptr<Module>(new Module("Cminus code"));
         builder = new IRBuilder(nullptr, module.get());
         auto TyVoid = Type::get_void_type(module.get());
@@ -63,34 +71,34 @@ public:
         auto input_type = FunctionType::get(TyInt32, {});
         auto input_fun =
             Function::create(
-                    input_type,
-                    "input",
-                    module.get());
+                input_type,
+                "input",
+                module.get());
 
         std::vector<Type *> output_params;
         output_params.push_back(TyInt32);
         auto output_type = FunctionType::get(TyVoid, output_params);
         auto output_fun =
             Function::create(
-                    output_type,
-                    "output",
-                    module.get());
+                output_type,
+                "output",
+                module.get());
 
         std::vector<Type *> output_float_params;
         output_float_params.push_back(TyFloat);
         auto output_float_type = FunctionType::get(TyVoid, output_float_params);
         auto output_float_fun =
             Function::create(
-                    output_float_type,
-                    "outputFloat",
-                    module.get());
+                output_float_type,
+                "outputFloat",
+                module.get());
 
         auto neg_idx_except_type = FunctionType::get(TyVoid, {});
         auto neg_idx_except_fun =
             Function::create(
-                    neg_idx_except_type,
-                    "neg_idx_except",
-                    module.get());
+                neg_idx_except_type,
+                "neg_idx_except",
+                module.get());
 
         scope.enter();
         scope.push("input", input_fun);
@@ -99,9 +107,11 @@ public:
         scope.push("neg_idx_except", neg_idx_except_fun);
     }
 
-    std::unique_ptr<Module> getModule() {
+    std::unique_ptr<Module> getModule()
+    {
         return std::move(module);
     }
+
 private:
     virtual void visit(ASTProgram &) override final;
     virtual void visit(ASTNum &) override final;
@@ -123,5 +133,8 @@ private:
     IRBuilder *builder;
     Scope scope;
     std::unique_ptr<Module> module;
+
+    // Use stack to evaluate expressions
+    std::stack<Value *> cal_stack;
 };
 #endif
