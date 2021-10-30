f8d624736d664176cda88bd2cafa36d90b3342b1
Ryan <ryanyuan.yyr@outlook.com>
2021-10-30 20:50:10 +0800

    Add function tables

--- include/cminusf_builder.hpp
+++ include/cminusf_builder.hpp
@@ -9,16 +9,22 @@
 #include "ast.hpp"
 #include <map>
 #include <stack>
+#include <utility>
+#include <functional>
+#include <algorithm>
 
-/**
- * Return module;
- * The type is Module *
- */
+using namespace std::placeholders;
+using ConvertorFuncType = std::function<void(Value *&)>;
+using CompFuncType = std::function<Value *(Value *left, Value *right)>;
+using AddFuncType = std::function<Value *(Value *left, Value *right)>;
+using MulFuncType = std::function<Value *(Value *left, Value *right)>;
+
 #define MOD this->module.get()
 
 #define GET_INT32 Type::get_int32_type(MOD)
 #define GET_FLOAT Type::get_float_type(MOD)
 #define GET_VOID Type::get_void_type(MOD)
+#define GET_BOOL Type::get_int1_type(MOD)
 
 class Scope
 {
@@ -59,7 +65,6 @@ public:
                 return iter->second;
             }
         }
-
         return nullptr;
     }
 
@@ -115,9 +120,77 @@ public:
         scope.push("output", output_fun);
         scope.push("outputFloat", output_float_fun);
         scope.push("neg_idx_except", neg_idx_except_fun);
+
+        compulsiveTypeConvertTable = {{{GET_INT32, GET_BOOL}, ConvertorFuncType([&](Value *&origin)
+                                                                                { origin = builder->create_icmp_ne(origin, ConstantInt::get(0, MOD)); })},
+                                      {{GET_FLOAT, GET_BOOL}, ConvertorFuncType([&](Value *&origin)
+                                                                                { origin = builder->create_fcmp_ne(origin, ConstantFP::get(0, MOD)); })},
+                                      {{GET_BOOL, GET_INT32}, ConvertorFuncType([&](Value *&origin)
+                                                                                { origin = builder->create_zext(origin, GET_INT32); })},
+                                      {{GET_FLOAT, GET_INT32}, ConvertorFuncType([&](Value *&origin)
+                                                                                 { origin = builder->create_fptosi(origin, GET_INT32); })},
+                                      {{GET_BOOL, GET_FLOAT}, ConvertorFuncType([&](Value *&origin)
+                                                                                { origin = builder->create_sitofp(builder->create_zext(origin, GET_INT32), GET_FLOAT); })},
+                                      {{GET_INT32, GET_FLOAT}, ConvertorFuncType([&](Value *&origin)
+                                                                                 { origin = builder->create_sitofp(origin, GET_FLOAT); })}};
+        typeOrderRank = {{GET_BOOL, 0},
+                         {GET_INT32, 1},
+                         {GET_FLOAT, 2}};
+        compFuncTable = {
+            {{GET_INT32, OP_LT}, [&](Value *left, Value *right)
+             { return builder->create_icmp_lt(left, right); }},
+            {{GET_INT32, OP_LE},
+             [&](Value *left, Value *right)
+             { return builder->create_icmp_le(left, right); }},
+            {{GET_INT32, OP_EQ},
+             [&](Value *left, Value *right)
+             { return builder->create_icmp_eq(left, right); }},
+            {{GET_INT32, OP_NEQ},
+             [&](Value *left, Value *right)
+             { return builder->create_icmp_ne(left, right); }},
+            {{GET_INT32, OP_GT},
+             [&](Value *left, Value *right)
+             { return builder->create_icmp_gt(left, right); }},
+            {{GET_INT32, OP_GE},
+             [&](Value *left, Value *right)
+             { return builder->create_icmp_ge(left, right); }},
+            {{GET_FLOAT, OP_LT}, [&](Value *left, Value *right)
+             { return builder->create_fcmp_lt(left, right); }},
+            {{GET_FLOAT, OP_LE},
+             [&](Value *left, Value *right)
+             { return builder->create_fcmp_le(left, right); }},
+            {{GET_FLOAT, OP_EQ},
+             [&](Value *left, Value *right)
+             { return builder->create_fcmp_eq(left, right); }},
+            {{GET_FLOAT, OP_NEQ},
+             [&](Value *left, Value *right)
+             { return builder->create_fcmp_ne(left, right); }},
+            {{GET_FLOAT, OP_GT},
+             [&](Value *left, Value *right)
+             { return builder->create_fcmp_gt(left, right); }},
+            {{GET_FLOAT, OP_GE},
+             [&](Value *left, Value *right)
+             { return builder->create_fcmp_ge(left, right); }}};
+        addFuncTable = {{{GET_INT32, OP_PLUS}, [&](Value *left, Value *right)
+                         { return builder->create_iadd(left, right); }},
+                        {{GET_INT32, OP_MINUS}, [&](Value *left, Value *right)
+                         { return builder->create_isub(left, right); }},
+                        {{GET_FLOAT, OP_PLUS}, [&](Value *left, Value *right)
+                         { return builder->create_fadd(left, right); }},
+                        {{GET_FLOAT, OP_MINUS}, [&](Value *left, Value *right)
+                         { return builder->create_fsub(left, right); }}};
+        mulFuncTable = {{{GET_INT32, OP_MUL}, [&](Value *left, Value *right)
+                         { return builder->create_imul(left, right); }},
+                        {{GET_INT32, OP_DIV}, [&](Value *left, Value *right)
+                         { return builder->create_isdiv(left, right); }},
+                        {{GET_FLOAT, OP_MUL}, [&](Value *left, Value *right)
+                         { return builder->create_fmul(left, right); }},
+                        {{GET_FLOAT, OP_DIV}, [&](Value *left, Value *right)
+                         { return builder->create_fdiv(left, right); }}};
     }
 
-    std::unique_ptr<Module> getModule()
+    std::unique_ptr<Module>
+    getModule()
     {
         return std::move(module);
     }
@@ -164,7 +237,65 @@ private:
         }
     }
 
+    bool isSelectStmt(ASTStatement *stmt)
+    {
+        return static_cast<bool>(dynamic_cast<ASTSelectionStmt *>(stmt));
+    }
+
+    bool isIterStmt(ASTStatement *stmt)
+    {
+        return static_cast<bool>(dynamic_cast<ASTIterationStmt *>(stmt));
+    }
+
+    bool isCtrlStmt(ASTStatement *stmt)
+    {
+        return isSelectStmt(stmt) || isIterStmt(stmt);
+    }
+
+    BasicBlock *newBasicBlock()
+    {
+        return BasicBlock::create(MOD, std::to_string(label_name_cnt++), lastEnteredFun);
+    }
+
+    std::map<std::pair<Type *, Type *>, ConvertorFuncType> compulsiveTypeConvertTable;
+
+    void compulsiveTypeConvert(Value *&origin, Type *target)
+    {
+        if (origin->get_type() != target)
+            compulsiveTypeConvertTable[{origin->get_type(), target}](origin);
+    }
+
+    Type *augmentTypeConvert(Value *&left, Value *&right)
+    {
+        auto resType = std::max(left->get_type(), right->get_type(), [&](Type *l, Type *r)
+                                { return typeOrderRank[l] < typeOrderRank[r]; });
+        compulsiveTypeConvert(left, resType);
+        compulsiveTypeConvert(right, resType);
+        return resType;
+    }
+
+    Type *augmentTypeConvert(Value *&left, Value *&right, Type *min)
+    {
+        auto resType = std::max({left->get_type(), right->get_type(), min}, [&](Type *l, Type *r)
+                                { return typeOrderRank[l] < typeOrderRank[r]; });
+        compulsiveTypeConvert(left, resType);
+        compulsiveTypeConvert(right, resType);
+        return resType;
+    }
+
+    std::map<Type *, int> typeOrderRank;
+    std::map<std::pair<Type *, enum RelOp>, CompFuncType> compFuncTable;
+    std::map<std::pair<Type *, enum AddOp>, AddFuncType> addFuncTable;
+    std::map<std::pair<Type *, enum MulOp>, MulFuncType> mulFuncTable;
+
     IRBuilder *builder;
+
+    /**
+     * @brief The types in scope can be
+     * AllocaInst
+     * GlobalVariable
+     * Function
+     */
     Scope scope;
     std::unique_ptr<Module> module;
     Function *lastEnteredFun = nullptr;
@@ -173,5 +304,8 @@ private:
     std::list<Argument *>::iterator curArg;
     // Use stack to evaluate expressions
     std::stack<Value *> cal_stack;
+    std::stack<BasicBlock *> bb_stack;
+
+    size_t label_name_cnt = 0;
 };
 #endif
