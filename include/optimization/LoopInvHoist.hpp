4cc5cfc79afeb2411170fc744ed5b72bd04e722c
serandt <serandt@LAPTOP-5AVALK47.localdomain>
2021-12-05 19:12:38 +0800

    Complete LoopInvHoist

--- include/optimization/LoopInvHoist.hpp
+++ include/optimization/LoopInvHoist.hpp
@@ -2,14 +2,23 @@
 
 #include <unordered_map>
 #include <unordered_set>
+#include <vector>
 #include "PassManager.hpp"
 #include "Module.h"
 #include "Function.h"
 #include "BasicBlock.h"
 
-class LoopInvHoist : public Pass {
+class LoopInvHoist : public Pass
+{
 public:
     LoopInvHoist(Module *m) : Pass(m) {}
 
     void run() override;
+
+private:
+    // 记录深度优先遍历过程中已经访问过的基本块
+    std::unordered_set<BasicBlock*> visited;
+    // 深度优先遍历循环的每个基本块
+    void dfs(BasicBlock* node, BBset_t *loop, LoopSearch loop_searcher);
+    bool can_delete(Instruction *instr, BBset_t *loop, std::vector<Instruction *>wait_delete);
 };
--- src/optimization/LoopInvHoist.cpp
+++ src/optimization/LoopInvHoist.cpp
@@ -10,4 +10,126 @@ void LoopInvHoist::run()
     loop_searcher.run();
 
     // 接下来由你来补充啦！
+    // 获取函数列表
+    auto func_list = m_->get_functions();
+    // 遍历所有函数
+    for (auto func : func_list)
+    {
+        if (func->get_basic_blocks().size() == 0)
+        {
+            continue;
+        }
+        else
+        {
+            // 获取函数的所有循环
+            std::unordered_set<BBset_t *> loops = loop_searcher.get_loops_in_func(func);
+
+            // 将循环按循环中块的个数排序，这样可以做到从最内存循环遍历所有循环
+            std::vector<BBset_t *> loops_sorted{loops.begin(), loops.end()};
+            std::sort(loops_sorted.begin(), loops_sorted.end(),
+                      [&](BBset_t *lhs, BBset_t *rhs)
+                      { return lhs->size() < rhs->size(); });
+
+            for (auto loop = loops_sorted.begin(); loop != loops_sorted.end(); loop++)
+            {
+                // printf("DEBUG:%d\n", (int)(*loop)->size());
+                auto base = loop_searcher.get_loop_base(*loop);
+                visited.clear();
+                dfs(base, (*loop), loop_searcher);
+            }
+        }
+    }
+}
+
+void LoopInvHoist::dfs(BasicBlock *node, BBset_t *loop, LoopSearch loop_searcher)
+{
+    std::vector<Instruction *> wait_delete;
+    visited.insert(node);
+    // 遍历基本块中的所有指令
+    for (auto instr = node->get_instructions().begin(); instr != node->get_instructions().end(); instr++)
+    {
+        if (can_delete(*instr, loop, wait_delete))
+            wait_delete.push_back(*instr);
+    }
+
+    // 找到循环外提的目标块
+    auto base = loop_searcher.get_loop_base(loop);
+    BasicBlock *target;
+    for (auto prev : base->get_pre_basic_blocks())
+        if (loop->find(prev) == loop->end())
+            target = prev;
+
+    // 将可外提的指令外提到目标块中
+    if (!wait_delete.empty())
+    {
+        // printf("DEBUG:can delete!\n");
+        for (auto instr = wait_delete.end() - 1; instr >= wait_delete.begin(); instr--)
+        {
+            target->add_instr_begin(*instr);
+            node->delete_instr(*instr);
+        }
+        wait_delete.clear();
+    }
+
+    // 接着深度优先遍历node的后继
+    for (auto succ = node->get_succ_basic_blocks().begin(); succ != node->get_succ_basic_blocks().end(); succ++)
+        if ((visited.find(*succ) == visited.end()) && (loop->find(*succ) != loop->end()))
+            dfs(*succ, loop, loop_searcher);
+}
+
+bool LoopInvHoist::can_delete(Instruction *instr, BBset_t *loop, std::vector<Instruction *> wait_delete)
+{
+    auto to_instruction = [&](Value *v)
+    { return dynamic_cast<Instruction *>(v); };
+    auto are_instruction = [&](Value *lhs, Value *rhs)
+    { return to_instruction(lhs) && to_instruction(rhs); };
+    auto to_const = [&](Value *v)
+    { return dynamic_cast<ConstantInt *>(v) || dynamic_cast<ConstantFP *>(v); };
+    auto are_const = [&](Value *lhs, Value *rhs)
+    { return to_const(lhs) && to_const(rhs); };
+
+    // std::cout << "DEBUG:" << instr->get_instr_op_name() << std::endl;
+    if (instr->isBinary())
+    {
+        auto lhs = instr->get_operand(0);
+        auto rhs = instr->get_operand(1);
+        bool can_l = false, can_r = false;
+        if (to_const(lhs))
+            can_l = true;
+        if (to_instruction(lhs))
+        {
+            auto lparent = to_instruction(lhs)->get_parent();
+            if (loop->find(lparent) == loop->end())
+                can_l = true;
+            else
+            {
+                for (auto del : wait_delete)
+                {
+                    if (del == to_instruction(lhs))
+                        can_l = true;
+                }
+            }
+        }
+
+        if (to_const(rhs))
+            can_r = true;
+        if (to_instruction(rhs))
+        {
+            auto rparent = to_instruction(rhs)->get_parent();
+            if (loop->find(rparent) == loop->end())
+                can_r = true;
+            else
+            {
+                for (auto del : wait_delete)
+                {
+                    if (del == to_instruction(lhs))
+                        can_r = true;
+                }
+            }
+        }
+
+        return (can_l && can_r);
+    } // if is_Binary
+    else
+        return false;
 }
